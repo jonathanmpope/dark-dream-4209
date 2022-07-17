6306d7c95d7a685821a41a7d42f8c5c311f6ac09
Jonathan Pope <jonathanmpope@gmail.com>
2022-07-17 17:09:56 -0600

    feat: user story 2 with tests - recipe show page

--- spec/features/recipes/index_spec.rb
+++ spec/features/recipes/index_spec.rb
@@ -1,39 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :feature do 
-    it 'has an index page with a list of recipes with recipe names' do 
-        recipe1 = Recipe.create!(name: "Spaghetti", complexity: 2, genre: "Italian")
-        recipe2 = Recipe.create!(name: "Tacos", complexity: 1, genre: "Mexican")
-        recipe3 = Recipe.create!(name: "Cheeseburger", complexity: 3, genre: "American")
+    describe 'index page' do 
+        it 'has an index page with a list of recipes with recipe names' do 
+            recipe1 = Recipe.create!(name: "Spaghetti", complexity: 2, genre: "Italian")
+            recipe2 = Recipe.create!(name: "Tacos", complexity: 1, genre: "Mexican")
+            recipe3 = Recipe.create!(name: "Cheeseburger", complexity: 3, genre: "American")
 
-        visit '/recipes'
+            visit '/recipes'
 
-        expect(page).to have_content(recipe1.name)
-        expect(page).to have_content(recipe2.name)
-        expect(page).to have_content(recipe3.name)
-    end
-    
-     it 'has an index page with a list of recipes with recipe complexity' do 
-        recipe1 = Recipe.create!(name: "Spaghetti", complexity: 2, genre: "Italian")
-        recipe2 = Recipe.create!(name: "Tacos", complexity: 1, genre: "Mexican")
-        recipe3 = Recipe.create!(name: "Cheeseburger", complexity: 3, genre: "American")
+            expect(page).to have_content(recipe1.name)
+            expect(page).to have_content(recipe2.name)
+            expect(page).to have_content(recipe3.name)
+        end
+        
+        it 'has an index page with a list of recipes with recipe complexity' do 
+            recipe1 = Recipe.create!(name: "Spaghetti", complexity: 2, genre: "Italian")
+            recipe2 = Recipe.create!(name: "Tacos", complexity: 1, genre: "Mexican")
+            recipe3 = Recipe.create!(name: "Cheeseburger", complexity: 3, genre: "American")
 
-        visit '/recipes'
+            visit '/recipes'
 
-        expect(page).to have_content(recipe1.complexity)
-        expect(page).to have_content(recipe2.complexity)
-        expect(page).to have_content(recipe3.complexity)
-    end
+            expect(page).to have_content(recipe1.complexity)
+            expect(page).to have_content(recipe2.complexity)
+            expect(page).to have_content(recipe3.complexity)
+        end
 
-     it 'has an index page with a list of recipes with recipe names' do 
-        recipe1 = Recipe.create!(name: "Spaghetti", complexity: 2, genre: "Italian")
-        recipe2 = Recipe.create!(name: "Tacos", complexity: 1, genre: "Mexican")
-        recipe3 = Recipe.create!(name: "Cheeseburger", complexity: 3, genre: "American")
+        it 'has an index page with a list of recipes with recipe names' do 
+            recipe1 = Recipe.create!(name: "Spaghetti", complexity: 2, genre: "Italian")
+            recipe2 = Recipe.create!(name: "Tacos", complexity: 1, genre: "Mexican")
+            recipe3 = Recipe.create!(name: "Cheeseburger", complexity: 3, genre: "American")
 
-        visit '/recipes'
+            visit '/recipes'
 
-        expect(page).to have_content(recipe1.genre)
-        expect(page).to have_content(recipe2.genre)
-        expect(page).to have_content(recipe3.genre)
-    end
+            expect(page).to have_content(recipe1.genre)
+            expect(page).to have_content(recipe2.genre)
+            expect(page).to have_content(recipe3.genre)
+        end
+    end 
 end 
---
+++ spec/features/recipes/show_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :feature do 
+    describe 'show page' do 
+        it 'has a show page with ingredient names' do 
+            recipe1 = Recipe.create!(name: "Spaghetti", complexity: 2, genre: "Italian")
+            noodles = recipe1.ingredients.create!(name: "noodles", cost:3)
+            red_sauce = recipe1.ingredients.create!(name: "red sauce", cost:2)
+            mushrooms = recipe1.ingredients.create!(name: "mushrooms", cost:1)
+
+            visit "/recipes/#{recipe1.id}"
+
+            expect(page).to have_content(recipe1.name)
+            expect(page).to have_content(noodles.name)
+            expect(page).to have_content(red_sauce.name)
+            expect(page).to have_content(mushrooms.name)
+        end 
+    end 
+end 
